d659bdb2aa6f52e34007f002d849ac82734f2ab6
Alexey <104529234+vorbodio@users.noreply.github.com>
2024-01-17 12:01:13 +0300

    Add files via upload
    13. Roman to Integer

---
+++ roman_to_integer.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def roman_to_int(self, s: str) -> int:
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+
+        total = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+            else:
+                total += roman_to_int[s[i]]
+
+        # return total
+        print(total)
+
+
+def res():
+    inputs = [
+        'III',
+        'LVIII',
+        'MCMXCIV',
+    ]
+
+    test = Solution()
+
+    for elem in inputs:
+        test.roman_to_int(s=elem)
+        print('----')
+
+
+if __name__ == '__main__':
+    res()
